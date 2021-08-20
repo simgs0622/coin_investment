d61f5b120927843b29a040e959c1ce168dd50cfe
simgs0622 <50125059+simgs0622@users.noreply.github.com>
2021-08-20 23:28:55 +0900

    Update VB-trading-v2.2.py
    Secrete

--- VB-trading-v2.2.py
+++ VB-trading-v2.2.py
@@ -2,8 +2,8 @@ import time
 import pyupbit
 import datetime
 
-access = "OtkVxrcbD942wwx15frEyOM5PAGEhDaIXLBmi0lt"
-secret = "XYngSUtQTUxXBpXOopRh3Rtjc3xNbhS087LLdx71"
+access = ""
+secret = ""
 coin_list = ['KRW-BTC', 'KRW-LTC', 'KRW-ETC', 'KRW-ETH', 'KRW-EOS']
 coin_shortlist = ['BTC', 'LTC', 'ETC', 'ETH', 'EOS']
 
